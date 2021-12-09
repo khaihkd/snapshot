ee5f4320c922fed82bb6ab8caed4e711dbb5feb4
Do Minh Khai <khaihkd@gmail.com>
2021-12-09 17:31:36 +0700

    Update account.js

--- models/account.js
+++ models/account.js
@@ -5,15 +5,11 @@ const Schema = mongoose.Schema
 
 const Account = new Schema({
     hash: { type: String, unique: true },
-    pool0: Number,
-    pool1: Number,
-    pool2: Number,
-    pool3: Number,
-    pool4: Number,
-    balanceNumber: Number,
-    accountType: { type: String, index: true },
-    isSend: { type: Boolean, index: true, default: false },
-    hasBalance: { type: Boolean, index: true, default: false }
+    s1325: Number,
+    s1305: Number,
+    s1280: Number,
+    stb: Number,
+    convert2Drace: Number
 }, { timestamps: false })
 
 module.exports = mongoose.model('Account', Account)
